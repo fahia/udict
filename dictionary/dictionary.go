a10d5e16b9b3cce526facea2b3a7eac8cfc8b945
Fahia Mohamed <fahia.mohamed@shopify.com>
2018-04-01 18:37:41 -0400

    Initial commit. Barebones CLI in place with a subcommand in place as well as a how-to-use prompt.

---
+++ dictionary/dictionary.go
@@ -0,0 +1,6 @@
+package dictionary
+
+func Define(word string) (dictionary string) {
+	dictionary = "defined"
+	return dictionary
+}
---
+++ udict.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"udict/dictionary"
+)
+
+func main() {
+	defineCommand := flag.NewFlagSet("define", flag.ExitOnError)
+
+	listTextPtr := defineCommand.String("word", "", "Word to define. (Required)")
+
+	if len(os.Args) < 2 {
+		fmt.Println("error: word subcommand is required")
+		defineCommand.PrintDefaults()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "define":
+		defineCommand.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if defineCommand.Parsed() {
+		if *listTextPtr == "" {
+			defineCommand.PrintDefaults()
+			os.Exit(1)
+		}
+		definedWord := dictionary.Define(*listTextPtr)
+		fmt.Printf("word is: %s\n", definedWord)
+	}
+}
